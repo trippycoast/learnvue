d702acc7d28c83f512ec32ba675c86a9cd4ac260
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2021-09-15 10:01:13 +0300

    add css

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+require('./style.css');
 const logger = require('./log.js');
 logger.log("hello");
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,22 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+  module: {
+    rules: [
+        { 
+          test: /\.css$/,
+          use: ['style-loader','css-loader']
+        },
+    ],
+  },
   plugins: [
       new HtmlWebpackPlugin({ template: './src/index.html' })
     ],
