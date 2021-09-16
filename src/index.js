28cb4bfed0563cf8c01fceb9eeb6268d89f64f1d
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2021-09-16 11:38:12 +0300

    add twig templating and bootstrap example

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ require('./style.scss');
 const logger = require('./log.js');
 logger.log("hello");
 require('./index.ts');
+
+class hello {
+    hello(){
+        console.log("hello");
+    }
+}
+
+let helloObj = new hello();
+helloObj.hello();
