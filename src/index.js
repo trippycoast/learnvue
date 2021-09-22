a2ab27a4be952b8097d7bbcc90b2eef0710059dc
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2021-09-22 11:42:34 +0300

    add if and switch examples

--- src/index.js
+++ src/index.js
@@ -1,90 +1,34 @@
-let foo = 1;
-foo = foo + 2;
-foo += 2;
-foo++;
-foo--;
-foo = 8;
-foo = foo % 3;
-let str = 'hello';
-str = str + ' world' + '!';
-str = str.toUpperCase();
-str = str.substr(6);
-let array = [
-    'apple',
-    'cherry',
-    'pear'
-];
-array.push(23, Math.PI);
-array.pop();
-array[1] = 'apricot';
+let bool = true;
+bool = false;
 
-let obj = {
-    name: 'kaspar',
-    age: 28,
-    isMale: true,
-    "as ds": 'asdas'
-}
-let prop = 'name';
+let num = 1;
 
-obj.eyes = 'blue';
-
-
-
-function add(a, b){
-    return a + b;
-}
-
-// let sub = function (a, b){
-//     return a - b;
+// if(num > 10 && num < 100){
+//     console.log("suurem");
 // }
 
-let sub = (a, b) => {
-    return a - b;
+
+if(num > 10 ){
+    console.log("suurem");
+} else if(num == 10){
+    console.log("võrdne");
+} else {
+    console.log("väiksem");
 }
 
-// let result = add(2,3);
+let option = 6;
 
-obj = {
-    array: ['a', 'b'],
-    obj: {
-        name: 'kaspar',
-        age: 12
-    },
-    add(a, b) {
-        return a + b;
-    },
-    cat:'nuustik'
+switch(option) {
+    case 1: 
+        console.log('üks');
+        break;
+    case 2:
+        console.log('kaks');
+        break;
+    case 3: 
+    case 4:
+        console.log('kolm v neli');
+        break;
+    default: 
+        console.log('dunno lol');
 }
-function sayhello(){
-        console.log('hello');
-}
-setInterval(sayhello, 5000);
-
-array = [
-    {name: 'kaspar'},
-    {name: 'martin'}
-];
-
-array = [[[[[1]]]]];
-console.log(array[0][0][0][0][0]);
-
-class Person {
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-
-    whoAmI(){
-        console.log('Hi I am ' + this.name + ' and I am ' + this.age + ' years old');
-    } 
-}
-
-let person1 = new Person('kaspar', 28);
-console.log(person1.name);
-person1.whoAmI();
-person1.name = 'vello';
-let person2 = new Person('martin', 34);
-console.log(person2.name);
-person2.whoAmI();
-console.log(person1.name);
-person1.whoAmI();
