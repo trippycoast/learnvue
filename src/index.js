cb7bf491daae51e0786fc634791ffadb0e960ff0
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2021-09-23 09:45:14 +0300

    loops examples

--- src/index.js
+++ src/index.js
@@ -1,34 +1,49 @@
-let bool = true;
-bool = false;
-
-let num = 1;
-
-// if(num > 10 && num < 100){
-//     console.log("suurem");
-// }
-
-
-if(num > 10 ){
-    console.log("suurem");
-} else if(num == 10){
-    console.log("võrdne");
-} else {
-    console.log("väiksem");
+for(let i=0 ; i<10 ; i++){
+    console.log(i);
+}
+let num = 2;
+while(num<1000000) {
+    num*=2;
+    console.log(num);
 }
 
-let option = 6;
+num = 1;
 
-switch(option) {
-    case 1: 
-        console.log('üks');
-        break;
-    case 2:
-        console.log('kaks');
-        break;
-    case 3: 
-    case 4:
-        console.log('kolm v neli');
-        break;
-    default: 
-        console.log('dunno lol');
+while(num>1){
+    console.log('while:' + num);
 }
+
+do {
+    console.log('do:' + num);
+} while(num>1);
+
+let fruits = ['apple', 'cherry', 'pear'];
+
+fruits.forEach((fruit)=>{
+    fruit = 'baaah';
+    console.log('value:' + fruit);
+});
+
+let newFruits = fruits.map((fruit, key)=>{
+    return fruit+key;
+});
+console.log(newFruits);
+
+let nums = [1,2,3,4,5,6,7,8,9];
+let sum = nums.reduce((sum, value)=>{
+    return sum + value;
+});
+
+console.log(sum);
+
+nums = [5,2,3,7,4,1,9,6,8];
+nums.sort((a, b) => {
+    if(a>b){
+        return 1;
+    }
+    if(a<b){
+        return -1;
+    }
+    return 0;
+});
+console.log(nums);
