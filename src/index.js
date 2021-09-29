3b19029cc9d04fe4457a2fbae96e9860caf6d50e
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2021-09-29 10:58:40 +0300

    add list example

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ let Vue = require('vue/dist/vue.common.dev');
 new Vue({
     el: '#app',
     data: {
-        message: ''
+        message: '',
+        items: [
+            {name:'apples', isDone: false},
+            {name:'milk', isDone: true},
+            {name:'brownies', isDone: true},
+        ]
     },
     methods: {
         clicked(){
-            alert('clicked');
+            if(this.message.trim() != ''){
+                this.items.push({name: this.message, isDone: false})   
+            }
+            this.message = '';
         }
     }
 });
