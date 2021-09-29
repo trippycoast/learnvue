8a978f89196c0970fba7c0369d37e8774d57158b
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2021-09-29 09:27:13 +0300

    add vue

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-//leida koodiga järgnevate inimeste kesmine vanus 
-//ning väljastada see ning inimese nimi kelle vanus sellele kõige lähemal on
+let Vue = require('vue/dist/vue.common.dev');
 
-let people = [
-    {name: 'vello', age: 34},
-    {name: 'martin', age: 17},
-    {name: 'kaspar', age: 64},
-    {name: 'manivald', age: 58},
-    {name: 'maria', age: 23},
-    {name: 'virginia', age: 14},
-    {name: 'malle', age: 83},
-    {name: 'kati', age: 26},
-]
+new Vue({
+    el: '#app',
+    data: {
+        greeting: 'hello Vue!'
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const { VueLoaderPlugin } = require('vue-loader');
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -33,6 +33,10 @@ module.exports = {
           test: /\.twig$/,
           use: ['twig-loader']
         },
+        { 
+          test: /\.vue$/,
+          use: ['vue-loader']
+        },
         // {
         //   test: /\.m?js$/,
         //   exclude: /node_modules/,
@@ -46,18 +50,20 @@ module.exports = {
     ],
   },
   plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/views/pages/index.twig',
-        filename: 'index.html',
-        posts: [
-          {title:'Lorem ipsum dolor sit amet.', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eveniet, officia aut impedit minus hic quae asperiores a dolores sunt nulla, sapiente provident blanditiis explicabo! Eaque rem ducimus harum commodi!'},
-          {title:'Eaque harum perferendis cum quis!', content: 'Voluptate consequatur doloribus aspernatur distinctio facere, architecto dignissimos amet iste assumenda vitae facilis quidem. Assumenda atque, officia, repudiandae odit, cupiditate numquam dignissimos culpa voluptatibus expedita libero sequi laudantium necessitatibus soluta?'},
-          {title:'Delectus obcaecati aspernatur magni ullam!', content: 'Qui esse, minima voluptas beatae exercitationem reprehenderit aliquam sed incidunt eius amet laborum accusantium perferendis ipsa nulla porro aperiam assumenda placeat, ab nihil saepe corrupti. Quae officiis necessitatibus laboriosam non.'},
-          {title:'Ullam quod officia vel rem!', content: 'Assumenda magnam delectus dicta nisi voluptatum maxime temporibus illo vitae dolores voluptas deserunt, quidem a explicabo odit accusamus dolore nulla debitis, libero quaerat beatae optio at vero facilis? Fugiat, error.'},
-          {title:'Quasi ratione quae voluptate. Officia.', content: 'Vel eaque eius impedit fuga consequatur sequi debitis fugiat cumque at, neque ex, quibusdam voluptatibus est quasi officiis, consectetur dolor dolorum voluptas illo architecto voluptatem. Consequuntur ullam totam asperiores fuga.'},
-        ]
-      }),
-      new HtmlWebpackPlugin({ template: './src/views/pages/about.twig', filename: 'about.html' }),
-      new HtmlWebpackPlugin({ template: './src/views/pages/contact.twig', filename: 'contact.html' })
+      // new HtmlWebpackPlugin({
+      //   template: './src/views/pages/index.twig',
+      //   filename: 'index.html',
+      //   posts: [
+      //     {title:'Lorem ipsum dolor sit amet.', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eveniet, officia aut impedit minus hic quae asperiores a dolores sunt nulla, sapiente provident blanditiis explicabo! Eaque rem ducimus harum commodi!'},
+      //     {title:'Eaque harum perferendis cum quis!', content: 'Voluptate consequatur doloribus aspernatur distinctio facere, architecto dignissimos amet iste assumenda vitae facilis quidem. Assumenda atque, officia, repudiandae odit, cupiditate numquam dignissimos culpa voluptatibus expedita libero sequi laudantium necessitatibus soluta?'},
+      //     {title:'Delectus obcaecati aspernatur magni ullam!', content: 'Qui esse, minima voluptas beatae exercitationem reprehenderit aliquam sed incidunt eius amet laborum accusantium perferendis ipsa nulla porro aperiam assumenda placeat, ab nihil saepe corrupti. Quae officiis necessitatibus laboriosam non.'},
+      //     {title:'Ullam quod officia vel rem!', content: 'Assumenda magnam delectus dicta nisi voluptatum maxime temporibus illo vitae dolores voluptas deserunt, quidem a explicabo odit accusamus dolore nulla debitis, libero quaerat beatae optio at vero facilis? Fugiat, error.'},
+      //     {title:'Quasi ratione quae voluptate. Officia.', content: 'Vel eaque eius impedit fuga consequatur sequi debitis fugiat cumque at, neque ex, quibusdam voluptatibus est quasi officiis, consectetur dolor dolorum voluptas illo architecto voluptatem. Consequuntur ullam totam asperiores fuga.'},
+      //   ]
+      // }),
+      // new HtmlWebpackPlugin({ template: './src/views/pages/about.twig', filename: 'about.html' }),
+      // new HtmlWebpackPlugin({ template: './src/views/pages/contact.twig', filename: 'contact.html' })
+      new HtmlWebpackPlugin({ template: './src/index.html'}),
+      new VueLoaderPlugin(),
     ],
 };
