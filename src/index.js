575a2dda12ac5e2420c3d6bb4f10fbdc92a364d3
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2021-10-07 11:02:27 +0300

    remove unused code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 let Vue = require('vue/dist/vue.common.dev');
 require('bulma');
-//const App = require('./components/App.vue'); // see ei tööta
+
 import App from './components/App.vue';
 
 Vue.component('app', App);
@@ -8,10 +8,3 @@ Vue.component('app', App);
 new Vue({
     el: '#app'    
 });
-
-
-// let input = document.getElementById('input');
-// let text = document.getElementById('text');
-// input.addEventListener('input', (event)=> {
-//     text.innerHTML = input.value;
-// })
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-let sometext:string = 'text';
-
-function log(text: string):void {
-    console.log(text);
-}
-
-log(sometext);
